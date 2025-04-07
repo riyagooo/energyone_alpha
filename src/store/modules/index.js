8f330ca624bda2ebd2580d61f00e8a690c0e9c49
lucostus <cleo@cwinkler.net>
2025-04-07 14:54:20 +0200

    init

--- src/store/modules/index.js
+++
@@ -1,8 +0,0 @@
-// src/store/modules/index.js
-export { useAuthStore } from './auth'
-export { useProjectsStore } from './projects'
-export { useRiskManagerStore } from './riskManager'
-export { useAdvisorsStore } from './advisors'
-export { useMessagesStore } from './messages'
-export { useSettingsStore } from './settings'
-export { useOnboardingStore } from './onboarding' 
